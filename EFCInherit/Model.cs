e5ec865ebd6f39b027670ac1846914c7df3727e4
Ranny Meier <Ranny@live.com>
2021-12-06 14:48:52 -0600

    Add project files.

---
+++ EFCInherit/Model.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCInherit
+{
+
+    public partial class ItemContext : DbContext
+    {
+        public ItemContext()
+        {
+        }
+
+        public ItemContext(DbContextOptions<ItemContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<Item> Items { get; set; } = null!;
+
+        public virtual DbSet<BomItem> BomItems { get; set; } = null!;
+
+        public virtual DbSet<SLItem> SLItems { get; set; } = null!;
+
+    }
+
+
+        public abstract partial class Item
+    {
+
+        public Item()
+        {
+            Name = "Name";
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    public partial class BomItem : Item
+    {
+        public BomItem()
+        {
+            SLItems = new HashSet<SLItem>();
+        }
+
+        public string Sku { get; set; } = null!;
+        //public int Id { get; set; }
+
+        public virtual Item IdNavigation { get; set; } = null!;
+        public virtual ICollection<SLItem> SLItems { get; set; }
+    }
+
+    public partial class SLItem : Item
+    {
+        public SLItem()
+        {
+        }
+
+        public string SerLot { get; set; } = null!;
+        public int BomItemId { get; set; }
+        //public int Id { get; set; }
+
+        public virtual BomItem BomItem { get; set; } = null!;
+        public virtual Item IdNavigation { get; set; } = null!;
+    }
+
+
+}
---
+++ EFCInherit/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EFCInherit;
+Console.WriteLine("Hello, World!");
+
+using ItemContext context = new ItemContext();
+{
+    BomItem bomItem = new();
+    context.BomItems.Add(bomItem);
+    SLItem sLItem = new() { BomItem = bomItem, SerLot = "1234" };
+    context.SLItems.Add(sLItem);
+
+
+}
